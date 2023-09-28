b1139cff40775247d6d510f833b5da7b419f6471
odutolaisreal <odutolaisreal11@gmail.com>
2023-09-28 09:02:46 +0100

    made some changes

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // alert("hi")
 
-const display = document.getElementById("inputText")
+const display = document.querySelector('.input')
 
-function calculator(number){
+const calculator = (number) =>{
   display.value+=number
+}
+const Result = () => {
+  try{
+    display.value = eval(display.value)
+  }
+  catch(err){
+    alert("Enter a valid number")
+  }
+}
+const Clear = () =>{
+  display.value=""
+}
+const Delete = () =>{
+  display.value = display.value.slice(0,-1)
+}
+const negative = () =>{
+  display.value = display.value*-1
 }
